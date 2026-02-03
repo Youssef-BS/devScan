797ab9edd5e42250827c19fba1b3adfd8900fdfa
youssef-bs <youssefbensaid827@gmail.com>
2026-02-03 10:53:58 +0100

    edit paths

--- apps/client/src/store/useServiceStore.ts
+++ apps/client/src/store/useServiceStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import type { AnalyseCardProps } from '@/types/Analyse';
+import  { AnalyseCardProps } from '@/types/Analyse';
 
 export type Service = AnalyseCardProps;
 
