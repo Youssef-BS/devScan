5bf3d26c38cfa2ee71ee3d4cdffdc439b64c8f53
youssef-bs <youssefbensaid827@gmail.com>
2026-02-10 14:48:33 +0100

    update useCommitStore and add alert for clear list

--- apps/client/src/app/dashboard/toCheck/page.tsx
+++ apps/client/src/app/dashboard/toCheck/page.tsx
@@ -1,9 +1,22 @@
 "use client"
 
-import React , { useEffect } from "react"
+import React, { useEffect } from "react"
 import { useRepoStore } from "@/store/useRepoStore"
 import RepoCard from "@/components/cards/RepoCard"
 import SpinnerLoad from "@/components/Spinner"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+import { Button } from "@/components/ui/button"
 
 const Page = () => {
   const {
@@ -17,28 +30,49 @@ const Page = () => {
 
   useEffect(() => {
     getFromDb()
-  }, [])
+  }, [getFromDb])
 
   return (
     <React.Fragment>
       <div className="justify-between m-16 flex items-center">
         <h1 className="font-bold text-2xl">My Projects to check</h1>
-        <button
-          onClick={deleteAllRepos}
-          className={`
-            bg-red-800 px-4 py-2 text-white rounded-lg  font-bold
-            ${dataFromDb.length === 0 ? 'opacity-30 cursor-not-allowed' : 'cursor-pointer'}
-            `}
-        >
-          Clear List
-        </button>
+
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button
+              disabled={dataFromDb.length === 0}
+              className="bg-red-800 hover:bg-red-700 text-white font-bold cursor-pointer"
+            >
+              Clear List
+            </Button>
+          </AlertDialogTrigger>
+
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you absolutely sure?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete all
+                repositories from your list.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+
+            <AlertDialogFooter>
+              <AlertDialogCancel className="cursor-pointer">Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={deleteAllRepos} className="bg-red-800 hover:bg-red-800 cursor-pointer">
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
 
       {loading ? (
         <SpinnerLoad />
       ) : dataFromDb.length > 0 ? (
         <section className="flex flex-wrap gap-5 m-16 justify-items-start">
-          {dataFromDb.map((repo , index) => (
+          {dataFromDb.map((repo, index) => (
             <RepoCard
               key={index}
               repo={repo}
