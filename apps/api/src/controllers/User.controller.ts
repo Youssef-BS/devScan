ae35afa01f8623ca418b656ecdba5e6806eb4e02
youssef-bs <youssefbensaid827@gmail.com>
2026-03-10 03:48:57 +0100

    update function get userDetails

--- apps/api/src/controllers/User.controller.ts
+++ apps/api/src/controllers/User.controller.ts
@@ -34,18 +34,34 @@ export const getListUsers = async (req: Request, res: Response) => {
 };
 
 export const getUserDetails = async (req: Request, res: Response) => {
-    const { id } = req.params ;
-    try {
-        const user = await prisma.user.findUnique({
-            where : { id : Number(id) } }
-        ) ;
+  const { id } = req.params;
 
-        user ?  res.status(200).json(user) : res.status(404).json({ message: 'User not found' });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: Number(id) },
+      include: {
+        repos: {
+          include: {
+            commits: {
+              include: {
+                files: true
+              }
+            }
+          }
+        }
+      }
+    });
 
-    }catch(error) {
-         res.status(500).json({ message: 'Internal server error' });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+
+    res.status(200).json(user);
+
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const banUser = async (req: Request, res: Response) => {
   const { id } = req.params;
