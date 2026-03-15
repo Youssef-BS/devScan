21d2b88c31dfd89d4102b1db3e8abb3956972ca1
youssef-bs <youssefbensaid827@gmail.com>
2026-03-15 17:53:49 +0100

    applicate isBanned in all routes

--- apps/api/src/routes/commit.routes.ts
+++ apps/api/src/routes/commit.routes.ts
@@ -6,11 +6,12 @@ import {
   analyzeCommitWithAI
 } from "../controllers/Commit.controller" ;
 import { auth } from "../middleware/auth.js" ;
+import { isBanned } from "src/middleware/isBanned";
 
 const router: Router = Router();
-router.get("/fetch/:githubId", auth, fetchAndSaveAllCommits);
-router.get("/repo/:githubId", auth, getAllCommits);
-router.get("/details/:sha", auth, getCommitDetails);
-router.post("/analyze", auth, analyzeCommitWithAI);
+router.get("/fetch/:githubId", auth, isBanned , fetchAndSaveAllCommits);
+router.get("/repo/:githubId", auth, isBanned , getAllCommits);
+router.get("/details/:sha", auth, isBanned , getCommitDetails);
+router.post("/analyze", auth, isBanned , analyzeCommitWithAI);
 
 export default router;
